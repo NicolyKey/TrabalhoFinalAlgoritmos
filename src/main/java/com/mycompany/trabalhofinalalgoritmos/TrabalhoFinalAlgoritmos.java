c9231342eabb397fad07d9e61fb6129bf16e7735
Nicoly Araujo <nicoly.araujo@premiersoft.net>
2025-04-09 15:01:28 -0300

    initializing

---
+++ src/main/java/com/mycompany/trabalhofinalalgoritmos/TrabalhoFinalAlgoritmos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhofinalalgoritmos;
+
+/**
+ *
+ * @author Premiersoft
+ */
+public class TrabalhoFinalAlgoritmos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
